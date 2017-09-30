08a73217a2c0fc8dcb982301d9dfdc94d9783bb1
eltxabi <eltxabi@gmail.com>
2017-09-30 15:56:27 +0200

    primero

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
-
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
+    url(r'', include('lftragos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
